a85d76c60a242f233109e5103ab2ec65ae857128
Chett Tiller <chett.tiller@gmail.com>
2018-09-25 21:53:15 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -1,45 +1,58 @@
-#Here is the song hash you will be working with. Each key is a song name and each value is the location of it's mp3 file.
-#make sure to edit the value of each key to replace < path to this directory >
-#with the correct path to this directory on your computer
-
-# my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
-# }
 
 def help
-  #this method should be the same as in jukebox.rb
-
+  puts "\nI accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end
 
-
-
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
-  #list the songs by name
+  i = 1
+  my_songs.each do |song,path|
+    puts "#{i}. #{song}"
+    i += 1
+  end
 end
 
 
 def play(my_songs)
-  #this method is slightly different!
-  #you should still ask the user for input and collect their song choice
-  #this time, only allow user's to input a song name
-  #check to see if the name they give is in fact a key of the my_songs hash
-  #if it isn't, tell them their choice is invalid
-  #if it is, play the song using the system 'open <file path>' syntax
-  #get the file path of the song by looking it up in the my_songs hash
-  
+  puts "\nPlease enter a song name:"
+  input = gets.chomp
+  done = false
+  my_songs.each do |song,path|
+    if song.downcase == input.downcase && done == false
+      puts "\nPlaying #{input}"
+      puts "#{path}"
+      done = true
+      system "open #{path}"
+    end
+  end
+  puts "\nInvalid input, please try again" if done == false
 end
+
+
 
 def exit_jukebox
-  #this method is the same as in jukebox.rb
+  puts "\nGoodbye"
 end
 
-def run(my_songs)
-  #this method is the same as in jukebox.rb
+def run(songs)
+  input = ""
+  help
+  while input != "exit"
+    puts "\nPlease enter a command:"
+    input = gets.chomp
+    case input
+    when "help"
+      help
+    when "list"
+      list(songs)
+    when "play"
+      play(songs)
+    else
+      "Invalid input"
+    end
+  end
+  exit_jukebox
 end
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,41 @@
+def help
+  puts "\nI accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def play(songs)
+  puts "\nPlease enter a song name or number:"
+  input = gets.chomp
+  done = false
+  songs.each do |song|
+    if song.downcase == input.downcase && done == false
+      puts "\nPlaying #{input}"
+      done = true
+    end
+  end
+  if (1..songs.length).include?(input.to_i)
+    input = input.to_i - 1
+    puts "\nPlaying #{songs[(input)]}"
+    done = true
+  end
+  puts "\nInvalid input, please try again" if done == false
+end
+
+def list(songs)
+  i = 0
+  songs.each do |song|
+    i += 1
+    puts "#{i}. #{song}"
+  end
+end
+
+def exit_jukebox
+  puts "\nGoodbye"
+end
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,3 +48,22 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def run(songs)
+  input = ""
+  help
+  while input != "exit"
+    puts "\nPlease enter a command:"
+    input = gets.chomp
+    case input
+    when "help"
+      help
+    when "list"
+      list(songs)
+    when "play"
+      play(songs)
+    else
+      "Invalid input"
+    end
+  end
+  exit_jukebox
+end
