bb113e562b71f467beae9d916a8e9b9ee5ee0880
Cara Morelli <caramorelli7@gmail.com>
2018-09-28 20:07:32 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,44 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  options = ['help', 'list', 'play', 'run', 'exit'] 
+  options.each { |opt| puts opt}
+end 
+
+def play(songs)
+  # puts "Enter a song name or number: "
+  input = gets.chomp 
+  if songs.include?(input)
+    puts input 
+    return
+  elsif input.match(/^(\d)+$/) && (input.to_i - 1) < songs.length
+    puts songs[(input.to_i) - 1]
+    return
+  end 
+  puts "Invalid input, please try again"
+end 
+
+def list(songs)
+  puts songs
+end 
+
+def exit_jukebox
+  puts 'Goodbye'
+end 
+
+def run(songs)
+  puts "Please enter a command:"
+  input = gets.chomp
+  case input 
+  when 'help'
+    help 
+  when 'list'
+    list(songs)
+  when 'play'
+    play(songs)
+  when 'exit'
+    exit_jukebox
+    exit 
+  end 
+end 
